1e3d523722c28cb13b029b809c83f86a59e319f7
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-19 01:29:55 +0200

    my printf

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+char *_strrev(char *str);
+char *_itoa(int value, char *strout, int base);
+
+#endif
+

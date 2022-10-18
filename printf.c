fd573b110c06850873c9252b9a45f99f00880125
NadineSafwat <nadinhassann@yahoo.com>
2022-10-18 15:27:16 -0700

    Revert "fixing betty problems in printf.c file"
    This reverts commit 8e6fe4f46ed541a80be3b6a0193d8123458b881a.

--- printf.c
+++ printf.c
@@ -28,22 +28,26 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
-			{
-				case 'c': {
-					buff[j] = (char) va_arg(ap, int);
-					j++;
-					break; }
+			switch (format[i]) {
+				case 'c':
+					{
+						buff[j] = (char) va_arg(ap, int);
+						j++;
+						break;
+					}
 				case 'd': {
-					_itoa(va_arg(ap, int), tmp, 10);
-					strcpy(&buff[j], tmp);
-					j += strlen(tmp);
-					break; }
-				case 's': {
-					str_arg = va_arg(ap, char*);
-					strcpy(&buff[j], str_arg);
-					j += strlen(str_arg);
-					break; }
+					 	 _itoa(va_arg(ap, int), tmp, 10);
+					 	 strcpy(&buff[j], tmp);
+					 	 j += strlen(tmp);
+						  break;
+				 	 }
+				case 's':
+				 	 {
+					 	 str_arg = va_arg(ap, char*);
+					 	 strcpy(&buff[j], str_arg);
+					  	j+= strlen(str_arg);
+					  	break;
+				  	}
 			}
 		}
 		else
